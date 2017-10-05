4b4649186521b0ad82c029482e0ad267ab7a901a
giselermf <gisele.ferreira@gmail.com>
2017-10-05 23:33:27 +0200

    refactoring

--- src/Main.vue
+++ src/Main.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="container">
     <tabs animation="slide" :only-fade="false">
-      <tab-pane label="Home">Home</tab-pane>
-      <tab-pane label="Categories" selected>Categories
+      <tab-pane label="Home" selected>Home
+         <my-chart></my-chart>
+
+      </tab-pane>
+      <tab-pane label="Categories">Categories
             <category></category>
+      </tab-pane>
+      <tab-pane label="Bank Records">Bank Records
       </tab-pane>
       <tab-pane label="About">About</tab-pane>
     </tabs>
@@ -13,10 +18,11 @@
 <script>
   import { Tabs, TabPane } from 'vue-bulma-tabs'
   import Category from './components/Category/Category.vue'
+  import MyChart from './components/MyChart.vue'
 
   export default {
     components: {
-      Tabs,TabPane, Category
+      Tabs,TabPane, Category, MyChart
     }
   }
 </script>
