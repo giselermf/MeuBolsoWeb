96a6877334fc205331315d8a78d0de99fabf1658
giselermf <gisele.ferreira@gmail.com>
2018-05-10 00:19:24 +0200

    budget progress

--- client/components/budget_management/BudgetCell.vue
+++ client/components/budget_management/BudgetCell.vue
@@ -2,20 +2,28 @@
 
     <td v-if="isHeader">{{element}}</td>
     <td v-else >
-      <input class="form-field-small" v-model="budgetValue" @change="saveBudget">
+      <input class="input budget-cell" v-model="budgetValue" @change="saveBudget">
       <label>{{actualsValue}}</label>
+      <progress :class="progressBarClass" :value="progressValue" :max="100"></progress>
+
     </td> 
 </template>
 
 <script>
+
 export default {
   props: ["element", "isHeader", "categoryId", "Month", "Year"],
   components: {},
   data() {
     return {
       budgetValue: this.getBudget(),
-      actualsValue: this.getActuals()
+      actualsValue: this.getActuals(),
+      progressValue: null,
+      progressBarClass: null
     };
+  },
+  created() {
+    this.getProgress();
   },
   methods: {
     saveBudget() {
@@ -43,15 +51,31 @@ export default {
     },
     getBudget() {
       if (this.element != undefined) {
-        return this.element.Budget;
+        return parseInt(this.element.Budget);
       }
       return 0;
     },
     getActuals() {
-      if (this.element != undefined) return this.element.Actuals;
+      if (this.element != undefined) return parseInt(this.element.Actuals);
       return 0;
+    },
+    getProgress() {
+      if (this.getBudget() != 0) this.progressValue = Math.abs(this.getActuals()/this.getBudget())*100;
+      else this.progressValue = 0;
+      this.getProgressBarClass();
+    },
+    getProgressBarClass() {
+      this.progressBarClass = 'progress is-small ';
+      if (this.progressValue > 70) this.progressBarClass += 'is-danger';
+      else if (this.progressValue > 50) this.progressBarClass += 'is-warning';
+      else this.progressBarClass += 'is-success';
     }
   }
 };
 </script>
  
+ <style>
+ .budget-cell {
+   max-width: 50%
+ }
+ </style>
